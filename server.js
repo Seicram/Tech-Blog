b78fc3cda20708646149cf46aa5e480624f254c0
marcies smith <marcies.smith@gmail.com>
2023-06-11 22:12:57 -0400

    fixing code

--- server.js
+++ server.js
@@ -1,54 +1,63 @@
-// TODO: Import dependencies (express, path, session, handlebars, controller routes, helpers)
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
-
-// TODO: Import sequelize connection
-const sequelize = require('./config/connection');
+const Sequelize = require('sequelize');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// TODO: Initialize an instance of Express.js and specifiy express PORT
+// Initialize an instance of Express.js and specify the express PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// TODO: Set up Handlebars.js engine with custom helpers for 'hbs'
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-// TODO: Set up session/cookie secret with Sequelize db connection
-const sess = {
-    secret: 'super secret',
-    cookie: {
-      maxAge: 90000000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  
-  app.use(session(sess));
- 
+// Set up session/cookie secret with Sequelize db connection
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+});
 
-// TODO: Inform Express.js on which template engine to use
+const sess = {
+  secret: 'super secret',
+  cookie: {
+    maxAge: 90000000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Set the template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Sets up the Express app to handle data parsing
+// Middleware for parsing JSON and URL-encoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
 app.use(routes);
 
-// TODO: Sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}! 🚀 `));
+// Sync Sequelize models to the database, then start the server
+sequelize.sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}! 🚀`));
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
   });
