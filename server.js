a888bde2c12d411aea4576e66ee4cdcf2f6ae334
marcies smith <marcies.smith@gmail.com>
2023-06-11 22:25:41 -0400

    Deploying to Heroku

--- server.js
+++ server.js
@@ -5,19 +5,17 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+
 const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, { dialect: 'mysql' });
+
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// Initialize an instance of Express.js and specify the express PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
-
-// Set up session/cookie secret with Sequelize db connection
-const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
-
 
 const sess = {
   secret: 'super secret',
@@ -36,25 +34,15 @@ const sess = {
 
 app.use(session(sess));
 
-// Set the template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Middleware for parsing JSON and URL-encoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Define routes
 app.use(routes);
 
-// Sync Sequelize models to the database, then start the server
-sequelize.sync({ force: false })
-  .then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}! 🚀`));
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}! 🚀 `));
+});
